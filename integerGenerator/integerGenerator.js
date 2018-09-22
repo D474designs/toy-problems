96b6ac2195786032a2488a895f2a5b5a432208de
Jacqueline930 <jacqueline.lopez.930@gmail.com>
2018-09-22 14:42:15 -0500

    integerGenerator

---
+++ integerGenerator/integerGenerator.js
@@ -0,0 +1,20 @@
+/**
+ * Given a function which produces a random integer in the range 1 to 5, write
+ * a function which produces a random integer in the range 1 to 7.
+ */
+
+// produces random integers in the range of [1,5] (inclusive)
+var random1to5 = function() {
+  // NOTE: You're allowed to use `Math.random()` in your solution.
+  // TODO: you're code here!
+  /* START SOLUTION */
+  /* END SOLUTION */
+};
+
+// produces random integers in the range [1,7] (inclusive)
+var random1to7 = function() {
+  // NOTE: Don't use `Math.random()` directly.
+  // TODO:  your code here!
+  /* START SOLUTION */
+  /* END SOLUTION */
+};
