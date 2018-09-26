2fd3a285ed6ec840d5dd588d3addc7d6e7897c00
Jacqueline930 <jacqueline.lopez.930@gmail.com>
2018-09-26 17:36:57 -0500

    isAnagram

---
+++ isAnagram/isAnagram.js
@@ -0,0 +1,30 @@
+/**
+ * Modify the String prototype to add a new method `isAnagram`.
+ * `isAnagram` takes a single string argument. It returns true if that string
+ * is an anagram of the string it was called on, and false otherwise.
+ *
+ * Example:
+ *  ("roasting").isAnagram("organist"); // true
+ *  ("presence").isAnagram("presents"); // false
+ *
+ * Anagrams should ignore spaces, punctuation, and capitalization:
+ *  ("Quid est veritas?").isAnagram("Est vir qui adest."); // true
+ *
+ * Extra credit: It is bad practice to extend a native prototype with enumerable
+ * properties. Make your isAnagram method non-enumerable.
+ *
+ * Extra extra credit: What is the complexity of your method in time and space?
+ * There is an algorithm that uses O(n) time and O(1) space.
+ *
+ */
+
+String.prototype.isAnagram = function(string) {
+  // TODO: Your code here!
+  /* START SOLUTION */
+
+  /* END SOLUTION */
+};
+
+/* START SOLUTION */
+
+/* END SOLUTION */
