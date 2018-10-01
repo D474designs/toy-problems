3f4233fbb80740a4b67e96bb6d5764ca99356da4
D474th3D357R0Y3R <jcazares117@gmail.com>
2018-10-01 09:27:30 -0500

    add commonAncestor alternate code

--- Common Ancestor/commonAncestor.js
+++ Common Ancestor/commonAncestor.js
@@ -1,3 +1,4 @@
+
 /**
   * implement the function `getClosestCommonAncestor` and `getAncestorPath`
   * in the following Tree class
@@ -28,37 +29,33 @@ Tree.prototype.addChild = function(child) {
   return this;
 };
 
-/**
-  * return the lowest common ancestor of the two child nodes.
-  * (assume for these examples that only a women can be the parent of a child)
-  * more examples:
-  *  1.) between me and my brother -> my mom
-  *  2.) between me and my cousin -> my grandma
-  *  3.) between my grandma and my grandma -> my grandma
-  *  4.) between me and a potato -> null
-  */
-Tree.prototype.getClosestCommonAncestor = function(/*...*//* START SOLUTION */ child1, child2 /* END SOLUTION */) {
-  // TODO: implement me!
-  /* START SOLUTION */
-  var path1 = this.getAncestorPath(child1);
-  if (!path1) {
+/**   * return the lowest common ancestor of the two child nodes.   * (assume
+for these examples that only a women can be the parent of a child)   * more
+examples:   *  1.) between me and my brother -> my mom   *  2.) between me and
+my cousin -> my grandma   *  3.) between my grandma and my grandma -> my
+grandma   *  4.) between me and a potato -> null   */
+Tree.prototype.getClosestCommonAncestor = function(node1, node2) {   // TODO: implement me!
+  // what does closest ancestor mean--> so given two nodes,
+  // get the ancestor path of each node, and if they have the same node (in the
+  // same position, would be ideal but not necessary), starting from the end
+  //return that node
+  // get ancestor path from this to each child
+  //the closest common ancestor is the point at which they diverge
+  var ancestorPathNode1 = this.getAncestorPath(node1);
+  var ancestorPathNode2 = this.getAncestorPath(node2);
+  if (!ancestorPathNode1 || !ancestorPathNode2) {
     return null;
   }
-
-  var path2 = this.getAncestorPath(child2);
-  if (!path2) {
-    return null;
-  }
-  var len = Math.min(path1.length, path2.length);
-  var closestAncestor = this;
-  for (var i = 0; i < len; i++) {
-    if (path1[i] === path2[i]) {
-      closestAncestor = path1[i];
+  var closestAncestor = null;
+  for (var i = 0; ancestorPathNode1[i] && ancestorPathNode2[i]; i++) {
+    // if there is an ancestorPath for one, but not the other, return this.children[i]
+    if (ancestorPathNode1[i] === ancestorPathNode2[i]) {
+      closestAncestor = ancestorPathNode1[i];
     }
   }
   return closestAncestor;
-  /* END SOLUTION */
 };
+
 
 /**
   * should return the ancestral path of a child to this node.
@@ -68,25 +65,22 @@ Tree.prototype.getClosestCommonAncestor = function(/*...*//* START SOLUTION */ c
   * 3.) me.getAncestorPath(me) -> [me]
   * 4.) grandma.getAncestorPath(H R Giger) -> null
   */
-Tree.prototype.getAncestorPath = function(/*...*//* START SOLUTION */ child, ancestors /* END SOLUTION */) {
+Tree.prototype.getAncestorPath = function(node) {
   // TODO: implement me!
-  /* START SOLUTION */
-  ancestors = ancestors || [];
-  if (this === child) {
-    // I'm the grandchild!
-    ancestors.unshift(this);
-    return ancestors;
-  }
-
-  for (var i = 0; i < this.children.length; i++) {
-    if (this.children[i].getAncestorPath(child, ancestors)) {
-      // one of my children contains the grandchild. add me to the list.
-      ancestors.unshift(this);
-      return ancestors;
+  // check if node is descendant of Tree, if yes, then recursively begin the search to identify the path
+    // iterate through the tree, and push the ancestor into the tree
+    // what is my condition? --DONE
+  if (this === node){
+    return [this]; //returning this vs. returning node, why?
+  } else {
+    for (var i = 0; i < this.children.length; i++) {
+      var pathFromChild=this.children[i].getAncestorPath(node);
+      if (pathFromChild) {
+        return [this].concat(pathFromChild);
+      }
     }
   }
   return null;
-  /* END SOLUTION */
 };
 
 /**
@@ -107,6 +101,138 @@ Tree.prototype.isDescendant = function(child) {
     return false;
   }
 };
+
+/**
+  * remove an immediate child
+  */
+Tree.prototype.removeChild = function(child) {
+  var index = this.children.indexOf(child);
+  if (index !== -1) {
+    // remove the child
+    this.children.splice(index, 1);
+  } else {
+    throw new Error('That node is not an immediate child of this tree');
+  }
+};
+
+ var grandma = new Tree();
+ var mom = new Tree();
+grandma.addChild(mom);
+var me = new Tree();
+mom.addChild(me);
+grandma.getAncestorPath(me);
+grandma.getClosestCommonAncestor(me, mom);
+
+
+// /**
+//   * implement the function `getClosestCommonAncestor` and `getAncestorPath`
+//   * in the following Tree class
+//   */
+//
+// /** example usage:
+//   * var grandma = new Tree();
+//   * var mom = new Tree();
+//   * grandma.addChild(mom);
+//   * var me = new Tree();
+//   * mom.addChild(me);
+//   * grandma.getAncestorPath(me); // => [grandma, mom, me]
+// */
+//
+// var Tree = function() {
+//   this.children = [];
+// };
+//
+// /**
+//   * add an immediate child
+//   */
+// Tree.prototype.addChild = function(child) {
+//   if (!this.isDescendant(child)) {
+//     this.children.push(child);
+//   } else {
+//     throw new Error('That child is already a child of this tree');
+//   }
+//   return this;
+// };
+//
+// /**
+//   * return the lowest common ancestor of the two child nodes.
+//   * (assume for these examples that only a women can be the parent of a child)
+//   * more examples:
+//   *  1.) between me and my brother -> my mom
+//   *  2.) between me and my cousin -> my grandma
+//   *  3.) between my grandma and my grandma -> my grandma
+//   *  4.) between me and a potato -> null
+//   */
+// Tree.prototype.getClosestCommonAncestor = function(/*...*//* START SOLUTION */ child1, child2 /* END SOLUTION */) {
+//   // TODO: implement me!
+//   /* START SOLUTION */
+//   var path1 = this.getAncestorPath(child1);
+//   if (!path1) {
+//     return null;
+//   }
+//
+//   var path2 = this.getAncestorPath(child2);
+//   if (!path2) {
+//     return null;
+//   }
+//   var len = Math.min(path1.length, path2.length);
+//   var closestAncestor = this;
+//   for (var i = 0; i < len; i++) {
+//     if (path1[i] === path2[i]) {
+//       closestAncestor = path1[i];
+//     }
+//   }
+//   return closestAncestor;
+//   /* END SOLUTION */
+// };
+//
+// /**
+//   * should return the ancestral path of a child to this node.
+//   * more examples:
+//   * 1.) greatGrandma.getAncestorPath(me) -> [great grandma, grandma, mom, me]
+//   * 2.) mom.getAncestorPath(me) -> [mom, me]
+//   * 3.) me.getAncestorPath(me) -> [me]
+//   * 4.) grandma.getAncestorPath(H R Giger) -> null
+//   */
+// Tree.prototype.getAncestorPath = function(/*...*//* START SOLUTION */ child, ancestors /* END SOLUTION */) {
+//   // TODO: implement me!
+//   /* START SOLUTION */
+//   ancestors = ancestors || [];
+//   if (this === child) {
+//     // I'm the grandchild!
+//     ancestors.unshift(this);
+//     return ancestors;
+//   }
+//
+//   for (var i = 0; i < this.children.length; i++) {
+//     if (this.children[i].getAncestorPath(child, ancestors)) {
+//       // one of my children contains the grandchild. add me to the list.
+//       ancestors.unshift(this);
+//       return ancestors;
+//     }
+//   }
+//   return null;
+//   /* END SOLUTION */
+// };
+//
+// /**
+//   * check to see if the provided tree is already a child of this
+//   * tree __or any of its sub trees__
+//   */
+// Tree.prototype.isDescendant = function(child) {
+//   if (this.children.indexOf(child) !== -1) {
+//     // `child` is an immediate child of this tree
+//     return true;
+//   } else {
+//     for (var i = 0; i < this.children.length; i++) {
+//       if (this.children[i].isDescendant(child)) {
+//         // `child` is descendant of this tree
+//         return true;
+//       }
+//     }
+//     return false;
+//   }
+// };
 
 /**
   * remove an immediate child
--- pigLatinConverter/pigLatinConverter.js
+++ pigLatinConverter/pigLatinConverter.js
@@ -23,4 +23,17 @@
 var convertToPigLatin = function(string) {
   // Convert the string to an array so it's easier to work with. Split on
   // whitespace.
+
+  // split string and push to an array
+
+  // if string begins with a vowel sound
+    // ad ay to the end of the string
+  // if string begins with a consonant
+    // take the first letter of the word and add it along with ay to the end
+
+  // return string(s)
 };
+
+  // extra credit:
+  // if string begins with a vowel sound remove ay and add to beginning
+  // if third to last char is a consonant remove ay and add it to the beginning
